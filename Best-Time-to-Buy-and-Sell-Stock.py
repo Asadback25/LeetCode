eb58d5d8725b23f5c033a413f14f5bc255540abc
Asadback25 <asadback25@gmail.com>
2025-10-04 21:53:39 +0500

    121. Best Time To Buy And Seel Stock / Easy

---
+++ Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,20 @@
+# LeetCode
+# 121. Best Time To Buy And Seel Stock / Easy
+# Time: O(n), Space: O(1)
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        L = 0
+        R = 1
+        maxx = 0
+        while R < len(prices):
+            if prices[L] < prices[R]:
+                profit = prices[R] - prices[L]
+                maxx = max(maxx, profit)
+            else:
+                L = R
+            R += 1
+        return maxx
+
+
+
