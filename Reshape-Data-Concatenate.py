795572a8c6d3513f95de3b708ebddc742540912a
Asadback25 <asadback25@gmail.com>
2025-10-10 00:39:49 +0500

    2888. Reshape Data: Concatenate / Easy

---
+++ Reshape-Data-Concatenate.py
@@ -0,0 +1,8 @@
+# LeetCode
+# 2888. Reshape Data: Concatenate / Easy
+# Pandas
+
+import pandas as pd
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+    return pd.concat([df1,df2], ignore_index=True)
