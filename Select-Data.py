ac7530d335a0fb4790ea16b274916225ca2f36cf
Asadback25 <asadback25@gmail.com>
2025-10-09 23:49:40 +0500

    2880. Select Data / Easy

---
+++ Select-Data.py
@@ -0,0 +1,8 @@
+# LeetCode
+# 2880. Select Data / Easy
+
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    res = students.loc[students['student_id']==101,['name','age']]
+    return res
