9b2fe16f91570d4be13501ca48d3bc62340c51c9
asadback25 <asadback25@gmail.com>
2025-11-03 21:10:22 +0500

    1578. Minimum Time to Make Rope Colorful / Medium

---
+++ LeetCode/Minimum-Time-to-Make-Rope-Colorful.py
@@ -0,0 +1,16 @@
+# LeetCode
+# 1578. Minimum Time to Make Rope Colorful / Medium
+
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        arr = []
+        for c, t in zip(colors, neededTime):
+            arr.append((c, t))
+
+        used = 0
+        for g, t in groupby(arr, key = lambda item: item[0]):
+            ct = list(nt for _, nt in t)
+            m = max(ct)
+            total = sum(ct)
+            used += (total - m)
+        return used
