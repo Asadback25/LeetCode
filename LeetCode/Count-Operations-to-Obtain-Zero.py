d3a29af20bcb81aba1a7050587aec427af43e4cb
asadback25 <asadback25@gmail.com>
2025-11-09 22:43:16 +0500

    2169. Count Operations to Obtain Zero / Easy

---
+++ LeetCode/Count-Operations-to-Obtain-Zero.py
@@ -0,0 +1,15 @@
+# LeetCode
+# 2169. Count Operations to Obtain Zero / Easy
+
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count = 0
+        if num2 > num1:
+            num1, num2, = num2, num1
+
+        while num2 > 0:
+            num1 -= num2
+            if num2 > num1:
+                num1, num2 = num2, num1
+            count += 1
+        return count
