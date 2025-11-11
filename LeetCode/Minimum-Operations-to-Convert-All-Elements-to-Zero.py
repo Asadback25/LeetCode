bcf22214b8c642223e6fee73209048aff2a2d17f
asadback25 <asadback25@gmail.com>
2025-11-11 23:30:25 +0500

    3542. Minimum Operations to Convert All Elements to Zero / Medium

---
+++ LeetCode/Minimum-Operations-to-Convert-All-Elements-to-Zero.py
@@ -0,0 +1,17 @@
+# LeetCode
+# 3542. Minimum Operations to Convert All Elements to Zero / Medium
+
+
+class Solution:
+  def minOperations(self, nums: list[int]) -> int:
+    ans = 0
+    stack = [0]
+
+    for num in nums:
+      while stack and stack[-1] > num:
+        stack.pop()
+      if not stack or stack[-1] < num:
+        ans += 1
+        stack.append(num)
+
+    return ans
