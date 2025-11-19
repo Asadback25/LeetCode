4d1f5c23a0931704fbaf9f50136bba960dd35e25
asadback25 <asadback25@gmail.com>
2025-11-19 11:02:52 +0500

    2154. Keep Multiplying Found Values by Two / Easy

---
+++ LeetCode/Keep-Multiplying-Found-Values-by-Two.py
@@ -0,0 +1,11 @@
+# LeetCode
+# 2154. Keep Multiplying Found Values by Two / Easy
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        nums.sort()
+
+        for i in range(len(nums)):
+            if nums[i] == original:
+                original *= 2
+        return original
