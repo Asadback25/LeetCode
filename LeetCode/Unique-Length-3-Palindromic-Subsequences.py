eaffa220ed3b09028da38c602c39c1ba87825b2c
asadback25 <asadback25@gmail.com>
2025-11-21 22:55:51 +0500

    1930. Unique Length-3 Palindromic Subsequences / Medium

---
+++ LeetCode/Unique-Length-3-Palindromic-Subsequences.py
@@ -0,0 +1,19 @@
+# LeetCode
+# 1930. Unique Length-3 Palindromic Subsequences / Medium
+
+class Solution:
+  def countPalindromicSubsequence(self, s: str) -> int:
+    ans = 0
+    first = [len(s)] * 26
+    last = [-1] * 26
+
+    for i, c in enumerate(s):
+      index = ord(c) - ord('a')
+      first[index] = min(first[index], i)
+      last[index] = i
+
+    for f, l in zip(first, last):
+      if f < l:
+        ans += len(set(s[f + 1:l]))
+
+    return ans
