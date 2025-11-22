e17f5463156c34ca74c961c4d023bec72e7f85a0
asadback25 <asadback25@gmail.com>
2025-11-22 23:00:32 +0500

    3190. Find Minimum Operations to Make All Elements Divisible by Three / Easy

---
+++ LeetCode/Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three.py
@@ -0,0 +1,11 @@
+# LeetCode
+# 3190. Find Minimum Operations to Make All Elements Divisible by Three / Easy
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        count = 0
+
+        for i in range(len(nums)):
+            if nums[i] % 3 != 0:
+                count += 1
+        return count
