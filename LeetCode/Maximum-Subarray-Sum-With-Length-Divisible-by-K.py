ee67d6cffba7293e26d77c82b0a3addd88599944
asadback25 <asadback25@gmail.com>
2025-11-27 22:52:47 +0500

    3381. Maximum Subarray Sum With Length Divisible by K / Medium

---
+++ LeetCode/Maximum-Subarray-Sum-With-Length-Divisible-by-K.py
@@ -0,0 +1,16 @@
+# LeetCode
+# 3381. Maximum Subarray Sum With Length Divisible by K / Medium
+
+class Solution:
+  def maxSubarraySum(self, nums: list[int], k: int) -> int:
+    ans = -math.inf
+    prefix = 0
+    minPrefix = [math.inf] * k
+    minPrefix[k - 1] = 0
+
+    for i, num in enumerate(nums):
+      prefix += num
+      ans = max(ans, prefix - minPrefix[i % k])
+      minPrefix[i % k] = min(minPrefix[i % k], prefix)
+
+    return ans
