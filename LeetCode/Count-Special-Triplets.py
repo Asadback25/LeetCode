52018563441ccc4e37fe6d9d9c9898d671dae450
asadback25 <asadback25@gmail.com>
2025-12-09 23:42:26 +0500

    3583. Count Special Triplets / Medium

---
+++ LeetCode/Count-Special-Triplets.py
@@ -0,0 +1,21 @@
+# LeetCode
+# 3583. Count Special Triplets / Medium
+
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        left_counter = Counter()
+        right_counter = Counter(nums)
+
+        result = 0
+        MOD = 10 ** 9 + 7
+
+        for current_num in nums:
+            right_counter[current_num] -= 1
+
+            double_value = current_num * 2
+            triplet_count = (left_counter[double_value] * right_counter[double_value]) % MOD
+            result = (result + triplet_count) % MOD
+
+            left_counter[current_num] += 1
+
+        return result
