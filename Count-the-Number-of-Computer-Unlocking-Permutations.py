a12bee3cf33416b5a51c58df6cca76200ee64297
asadback25 <asadback25@gmail.com>
2025-12-10 23:45:46 +0500

    3577. Count the Number of Computer Unlocking Permutations / Medium

---
+++ Count-the-Number-of-Computer-Unlocking-Permutations.py
@@ -0,0 +1,17 @@
+# LeetCode
+# 3577. Count the Number of Computer Unlocking Permutations / Medium
+
+
+class Solution:
+    def countPermutations(self, complexity: List[int]) -> int:
+        MOD = 10 ** 9 + 7
+
+        for i in range(1, len(complexity)):
+            if complexity[i] <= complexity[0]:
+                return 0
+
+        result = 1
+        for i in range(1, len(complexity)):
+            result = (result * i) % MOD
+
+        return resultA
